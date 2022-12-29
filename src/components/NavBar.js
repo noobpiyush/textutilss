fd4d84836761cca1c66824f8d94ba884cd9ce8e1
noobpiyush <83893659+noobpiyush@users.noreply.github.com>
2022-12-29 22:20:55 +0530

    Create NavBar.js
    nvenv

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,7 @@ export default function NavBar(props) {
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
   <div className="container-fluid">
     <a className="navbar-brand" href="#">{props.tittle}</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
      </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
